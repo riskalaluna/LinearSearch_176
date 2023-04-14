80d5acec783d3e809f396e2b5e36346d0644b8c7
riskalaluna <riskalaluna@gmail.com>
2023-04-14 10:59:49 +0700

    Add project files.

---
+++ LinearSearch_176/LinearSearch_176.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[arr]; //Array to be searched
+int n; //Number of elements in the array
+int i; //Index of array element
+
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array:"
+	}
+}
