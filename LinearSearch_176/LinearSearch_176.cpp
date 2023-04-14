aba5b5ad1aef3968fedf668e9f0867c1f3797c20
riskalaluna <riskalaluna@gmail.com>
2023-04-14 11:04:00 +0700

    accept array elemets

--- LinearSearch_176/LinearSearch_176.cpp
+++ LinearSearch_176/LinearSearch_176.cpp
@@ -15,6 +15,13 @@ void input()
 		cin >> n;
 		if ((n > 0) && (n <= 20))
 			break;
-
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+
+	// Accept array elements
+	cout << "\n------------------------\n";
+	cout << "Enter array elements \n";
+	cout << "-------------------------\n";
+	
 }
